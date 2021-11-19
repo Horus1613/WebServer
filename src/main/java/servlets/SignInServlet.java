16cc358b2c8ff3fdb20ea64d61a84b58771c0cb4
Doronin Alexsej <doronin.alexsej@gmail.com>
2021-11-19 16:07:06 +0300

    Initial commit

---
+++ src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignInServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignInServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        String login = request.getParameter("login");
+        String pass = request.getParameter("pass");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        if (!accountService.getUserByLogin(login).getLogin().equals(login) || !accountService.getUserByLogin(login).getPass().equals(pass)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Unauthorized");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+        if (accountService.getUserByLogin(login).getPass().equals(pass)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Authorized:" + accountService.getUserByLogin(login).getLogin());
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
+}
